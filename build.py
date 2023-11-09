5418fbc9fb4b2c3a060a41a77f0de35efa89eb19
Julian reyes Escrigas <julian.reyes.escrigas@gmail.com>
2023-11-09 17:44:24 -0500

    chore c build flags

--- build.py
+++ build.py
@@ -2,9 +2,11 @@ from typing import Any, Dict
 
 from setuptools import Extension
 
+cflags = ['-std=c99']
 ext_modules = [
     Extension(
         "streamdeck_uinput._uinput", ["streamdeck_uinput/uinput.c"],
+        extra_compile_args=cflags,
     ),
 ]
 
